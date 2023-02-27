eb080758a75fc255b9b5a155b9c431137a66cb21
Em Lindvall <elindvall22@turing.edu>
2023-02-27 14:55:46 -0600

    Refactor HTML, CSS, and JS with final stylistic changes

--- idea.js
+++ idea.js
@@ -1,5 +1,5 @@
 class Idea {
-    constructor(title, body){
+    constructor(title, body) {
         this.id = Date.now();
         this.title = title;
         this.body = body;
--- main.js
+++ main.js
@@ -16,16 +16,15 @@ var ideas = [];
 
 saveButton.addEventListener('click', function(event){
     event.preventDefault();
-  if (titleInput.value !== "" && bodyInput.value !== "") {
-      saveIdea();
-      updateCardOutput();
-      clearInputs();
-  } else {
-    inputWarning()
-    setTimeout(clearInputs, 2000)
+    if (titleInput.value !== "" && bodyInput.value !== "") {
+        saveIdea();
+        updateCardOutput();
+        clearInputs();
+    } else {
+        inputWarning();
+        setTimeout(clearInputs, 2000);
   }
-}
-);
+});
 
 outputContainer.addEventListener('click', function(){
     if (event.target.className.includes("delete")) {
@@ -35,8 +34,7 @@ outputContainer.addEventListener('click', function(){
         toggleStar();
         updateCardOutput();
     }
-}
-);
+});
 
 toggleStarredIdeasButton.addEventListener('click', function(){
     if (toggleStarredIdeasButton.innerText === "Show Starred Ideas") {
@@ -50,7 +48,7 @@ toggleStarredIdeasButton.addEventListener('click', function(){
 searchInput.addEventListener('keyup', function(){
     outputContainer.innerHTML = "";
     filterIdeas();
- });
+});
 
 
 // Functions
@@ -76,7 +74,7 @@ function deleteIdea() {
 function toggleStar() {
     for (i = 0; i < ideas.length; i++) {
         if (ideas[i].id == event.target.parentNode.id) {
-            ideas[i].updateIdea()
+            ideas[i].updateIdea();
         }
     }
 };
@@ -86,31 +84,31 @@ function updateCardOutput(){
   for (i = 0; i < ideas.length; i++) {
       var starred = ideas[i].star ? "assets/star-active.svg" : "assets/star.svg";
       outputContainer.innerHTML +=
-      `<article class="card-container">
-          <div class="container-top-nav" id="${ideas[i].id}">
-              <img class="star-icon icons" src="${starred}" alt="favorite idea"></img> 
-              <img class="delete-icon icons" src="assets/delete.svg" alt="delete idea"></img> 
-          </div>
-          <div class="idea-container">
-              <p class="title-output">${ideas[i].title}</p>
-              <p class="body-output">${ideas[i].body}</p>
-          </div>
-          <div class="container-bottom-nav">
-              <img class="plus-icon icons" src="assets/comment.svg" alt="add comment"></img> 
-              <p class="comment">Comment</p>
-          </div>
-      </article>`
+        `<article class="card-container">
+            <div class="container-top-nav" id="${ideas[i].id}">
+                <img class="star-icon icons" src="${starred}" alt="favorite idea"></img> 
+                <img class="delete-icon icons" src="assets/delete.svg" alt="delete idea"></img> 
+            </div>
+            <div class="idea-container">
+                <p class="title-output">${ideas[i].title}</p>
+                <p class="body-output">${ideas[i].body}</p>
+            </div>
+            <div class="container-bottom-nav">
+                <img class="plus-icon icons" src="assets/comment.svg" alt="add comment"></img> 
+                <p class="comment">Comment</p>
+            </div>
+        </article>`
       }
 };
 
 
 function showStarred() {
     toggleStarredIdeasButton.innerText = "Show All Ideas";
-        outputContainer.innerHTML = "";
-        for (i = 0; i < ideas.length; i++) {
-            if (ideas[i].star === true) {
-                var starred = ideas[i].star ? "assets/star-active.svg" : "assets/star.svg";
-                outputContainer.innerHTML +=
+    outputContainer.innerHTML = "";
+    for (i = 0; i < ideas.length; i++) {
+        if (ideas[i].star === true) {
+            var starred = ideas[i].star ? "assets/star-active.svg" : "assets/star.svg";
+            outputContainer.innerHTML +=
                 `<article class="card-container">
                     <div class="container-top-nav" id="${ideas[i].id}">
                         <img class="star-icon icons" src="${starred}" alt="favorite idea"></img> 
@@ -130,8 +128,8 @@ function showStarred() {
 };
 
 function inputWarning(){
-    titleInput.value = "Please enter a title for your idea!"
-    bodyInput.value = "Please enter your idea text!"
+    titleInput.value = "Please enter a title for your idea!";
+    bodyInput.value = "Please enter your idea text!";
 };
 
 function filterIdeas() {
@@ -139,20 +137,20 @@ function filterIdeas() {
         if (ideas[i].title.includes(searchInput.value) || ideas[i].body.includes(searchInput.value)) {
             var starred = ideas[i].star ? "assets/star-active.svg" : "assets/star.svg";
             outputContainer.innerHTML +=
-            `<article class="card-container">
-                <div class="container-top-nav" id="${ideas[i].id}">
-                    <img class="star-icon icons" src="${starred}" alt="favorite idea"></img>
-                    <img class="delete-icon icons" src="assets/delete.svg" alt="delete idea"></img>
-                </div>
-                <div class="idea-container">
-                    <p class="title-output">${ideas[i].title}</p>
-                    <p class="body-output">${ideas[i].body}</p>
-                </div>
-                <div class="container-bottom-nav">
-                    <img class="plus-icon icons" src="assets/comment.svg" alt="add comment"></img>
-                    <p class="comment">Comment</p>
-                </div>
-            </article>`
+                `<article class="card-container">
+                    <div class="container-top-nav" id="${ideas[i].id}">
+                        <img class="star-icon icons" src="${starred}" alt="favorite idea"></img>
+                        <img class="delete-icon icons" src="assets/delete.svg" alt="delete idea"></img>
+                    </div>
+                    <div class="idea-container">
+                        <p class="title-output">${ideas[i].title}</p>
+                        <p class="body-output">${ideas[i].body}</p>
+                    </div>
+                    <div class="container-bottom-nav">
+                        <img class="plus-icon icons" src="assets/comment.svg" alt="add comment"></img>
+                        <p class="comment">Comment</p>
+                    </div>
+                </article>`
         }
     }
  };
